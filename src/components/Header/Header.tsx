d60123538d11b769320bd612d43e1790a534f12f
AleksandrKhv <sidby83@gmail.com>
2022-02-06 16:53:07 +0100

    header_end

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import st from './Header.module.css'
+
+const Header = () => {
+    return (
+        <header>
+            <a href="#" className={st.logo}><i></i></a>
+        </header>
+    );
+};
+
+export default Header;
