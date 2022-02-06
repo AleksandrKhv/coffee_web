34508b337c7f136b5b8c9f2a212025ce044e136a
AleksandrKhv <sidby83@gmail.com>
2022-02-06 19:37:05 +0100

    htader+js

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import st from './Header.module.css'
+import logo from './../../images/logo.png'
+import Nav from '../Navbar/Nav';
 
 const Header = () => {
     return (
-        <header>
-            <a href="#" className={st.logo}><i></i></a>
+        <header className={st.header}>
+            <a href="#" className={st.logo}>
+                <img src={logo} alt=""/>
+            </a>
+            <Nav/>
         </header>
     );
 };
---
+++ src/components/Navbar/Nav.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import st from './Nav.module.css'
+import {FaSearch, FaShoppingCart, FaBars} from 'react-icons/fa'
+import cart_item_1 from './../../images/cart-item-1.png'
+import cart_item_2 from './../../images/cart-item-2.png'
+import cart_item_3 from './../../images/cart-item-3.png'
+import cart_item_4 from './../../images/cart-item-4.png'
+
+
+
+const Nav = () => {
+
+    return (
+        <>
+            <nav className={st.navbar}>
+                <a href="#home">home</a>
+                <a href="#about">about</a>
+                <a href="#menu">menu</a>
+                <a href="#products">products</a>
+                <a href="#review">review</a>
+                <a href="#contacts">contacts</a>
+                <a href="#blogs">blogs</a>
+            </nav>
+            <div className={st.icons}>
+                <div className={st.fas_fa_search} id={'search-btn'}><FaSearch/></div>
+                <div className={st.fas_fa_shopping_cart} id={'cart-btn'}><FaShoppingCart/></div>
+                <div className={st.fas_fa_bars} id={'menu-btn'}><FaBars/></div>
+            </div>
+            <div className={st.search_form}>
+                <input type={"search"} id={"search-box"} placeholder={"search here..."}/>
+                    <label htmlFor={"search-box"} className={st.fas_fa_search}></label>
+            </div>
+            <div className={st.cart_items_container}>
+                <div className={st.cart_item}>
+                    <span className={st.fas_fa_times}></span>
+                    <img src={cart_item_1} alt=""/>
+                        <div className={st.content}>
+                            <h3>cart item 01</h3>
+                            <div className={st.price}>$15.99/-</div>
+                        </div>
+                </div>
+                <div className={st.cart_item}>
+                    <span className={st.fas_fa_times}></span>
+                    <img src={cart_item_2} alt=""/>
+                        <div className={st.content}>
+                            <h3>cart item 02</h3>
+                            <div className={st.price}>$15.99/-</div>
+                        </div>
+                </div>
+                <div className={st.cart_item}>
+                    <span className={st.fas_fa_times}></span>
+                    <img src={cart_item_3} alt=""/>
+                        <div className={st.content}>
+                            <h3>cart item 03</h3>
+                            <div className={st.price}>$15.99/-</div>
+                        </div>
+                </div>
+                <div className={st.cart_item}>
+                    <span className={st.fas_fa_times}></span>
+                    <img src={cart_item_4} alt=""/>
+                        <div className={st.content}>
+                            <h3>cart item 04</h3>
+                            <div className={st.price}>$15.99/-</div>
+                        </div>
+                </div>
+                <a href="#" className={st.btn}>checkout now</a>
+            </div>
+        </>
+    );
+};
+
+export default Nav;
